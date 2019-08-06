30cc25aefa82b0d9133ab15eec71aad0d9eae1ec
siaki1 <siaki1@morgan.edu>
2019-08-06 09:58:31 -0400

    attempted

--- src/main/java/hbcu/stay/ready/algor/Solution.java
+++ src/main/java/hbcu/stay/ready/algor/Solution.java
@@ -1,8 +1,22 @@
 package hbcu.stay.ready.algor;
+import java.util.*;
 
 public class Solution {
 
-    public Boolean harmlessRansomNote(String message , String magazineText){
-        return null;
+    public Boolean harmlessRansomNote(String message , String magazineText) {
+
+        Map<Character, Integer> word1 = new HashMap<>();
+        for (char word2 : magazineText.toCharArray()) {
+            if (word1.containsKey(word2)) {
+                word1.put(word2, word1.get(word2) + 1);
+            } else {
+                word1.put(word2, 1);
+            }
+
+        }
+        return true;
+
+        
     }
-}
+    }
+
